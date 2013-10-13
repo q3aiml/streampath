0c048ca0a09384aebf8205b02741addabd6b36d6
Andy Clayton <q3aiml@gmail.com>
2013-10-12 20:27:03 -0700

    cli tester for parser

---
+++ src/test/java/net/q3aiml/streampath/lang/ParserCliTester.java
@@ -0,0 +1,46 @@
+package net.q3aiml.streampath.lang;
+
+import com.google.common.base.Strings;
+import org.parboiled.Parboiled;
+import org.parboiled.errors.ErrorUtils;
+import org.parboiled.parserunners.ReportingParseRunner;
+import org.parboiled.support.ParseTreeUtils;
+import org.parboiled.support.ParsingResult;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * @author q3aiml
+ */
+public class ParserCliTester {
+    private static Parser parser = Parboiled.createParser(Parser.class);
+
+    public static void main(String... args) throws IOException {
+        new ParserCliTester().run();
+    }
+
+    public void run() throws IOException {
+        while (true) {
+            String input = new Scanner(System.in).nextLine();
+            if (Strings.isNullOrEmpty(input)) break;
+
+            parseAndPrint(input);
+        }
+    }
+
+    public void parseAndPrint(String input) throws IOException {
+        ParsingResult result = new ReportingParseRunner(parser.Expression()).run(input);
+        String parseTreePrintOut = ParseTreeUtils.printNodeTree(result);
+        if (!result.hasErrors()) {
+            System.out.println(parseTreePrintOut);
+            System.out.println();
+        } else {
+            System.out.println(input);
+            System.out.println("parse errors:");
+            System.out.println(ErrorUtils.printParseErrors(result.parseErrors));
+            System.out.println();
+        }
+    }
+
+}
